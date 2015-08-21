78e5eb7b85881aeefa41f6947cf33f876857bd38
Tredok Vayntrub <tredok@Chloe-Machine>
2015-08-20 19:24:32 -0500

    Primer ejercicios hecho

--- P01/practica01.rkt
+++ P01/practica01.rkt
@@ -4,6 +4,12 @@
 (define (mlength a-lst)
   (cond
     [(empty? a-lst) 0]
-    [else (+1(mlength (cdr a-lst)))])
+    [else (+1(mlength (cdr a-lst)))] ))
 
+  
+;Ejercicio Pow
 
+(define (pow x w)
+  (cond
+    [(equal? 0 w) 1]
+    [else  (* x (pow x (- w 1)) )] ))
