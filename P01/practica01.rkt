dce28508dba035b478e08a85087fb995f2fc6d40
Tredok Vayntrub <tredok@Chloe-Machine>
2015-08-27 17:31:16 -0500

    Tercer ejercicio, pruebas para los ejercicios ya hechos y un intento de la función de mconcat

--- P01/practica01.rkt
+++ P01/practica01.rkt
@@ -6,17 +6,20 @@
     [(empty? a-lst) 0]
     [else (+1(mlength (cdr a-lst)))] ))
 
-  
+
 ; Ejercicio 1 Pow
 (define (pow x w)
   (cond
     [(equal? 0 w) 1]
     [else  (* x (pow x (- w 1)) )] ))
 ;pruebas
+
 (test (pow 3 0) 1)
 (test (pow 0 0) 1)
 (test (pow 0 4) 0)
 (test (pow 2 3) 8)
+
+
 
 
 ; Ejercicio 2 Average
@@ -39,23 +42,36 @@
 (test (average '(10 3 17)) 10)
 
 
-; Ejercicio 4 Zip INCOMPLETO
-;(define (zip list1 list2)
- ; (cond
-  ;  [(empty? list1) '()]
-   ; [(empty? list2) '()]
-   ; [else 
-     
-;Funcion aux para zip
-(define (auxzi list1 list2)
+
+
+;Función zip
+(define (zip list1 list2)
   (cond
-    [(null? list1) '()]
-    [(null? list2) '()]
-    [else (cond (car list1) (car list2))] ))
+    [(empty? list1) '()]
+    [(empty? list2) '()]
+    [else (cons (cons (car list1) (cons (car list2) empty)) (zip (cdr list1) (cdr list2)))] ))
+
+(test (zip '() '()) '())
+(test (zip '(2 4) '()) '())
+(test (zip '() '(4 2)) '())
+(test (zip '(4) '(3)) '((4 3)))
+(test (zip '(1 3 5) '(2 4 6)) '((1 2) (3 4) (5 6)))
+(test (zip '(1 3 5) '(2 4)) '((1 2) (3 4)))
+
+
+
 
 ;mconcat Ejercicio 1 Sección 2
 {define (mconcat list1 list2)
   (cond
-    [(null? list1) list2]
-    [(null? list2) list1]
-    [else (cons (car list1) (car list2)) (mconcat (cdr list1)(cdr list2))] )}
+    [(empty? list1) list2]
+    [(empty? list2) list1]
+    [else (cons (car list1) list2)])}
+  
+ ;  (test (mconcat '() '()) '())
+ ;  (test (mconcat '() '(3 1)) '(3 1))
+ ;  (test (mconcat '(4 6) '()) '(4 6))
+ ;  (test (mconcat '(3) '(5 1)) '(3 5 1))
+ ;  (test (mconcat '(3 4) '(5 1)) '(3 4 5 1))
+   
+   
