088007088f4187f66dd346b2a493cd80fa2c197d
kcontrerasm <karlaaw.kc@gmail.com>
2015-09-09 14:11:37 -0500

    Avances de la practica

--- P02/practica2.rkt
+++ P02/practica2.rkt
@@ -1,1 +1,30 @@
 #lang plai
+(define-type Array
+  [MArray (n number?)
+          (l list?)])
+
+(MArray 4 '(1 2 3)) 
+
+(define-type MList
+  [MEmpty]
+  [MCons (e any?)(l MList?)])
+
+;;Para aceptar elementos de cualquier tipo
+(define (any? e)#t)
+
+(define-type NTree
+  [TLEmpty]
+  [NodeN (n number?) (l listNTree?)])
+
+(define (listNTree? lst)
+(if (empty? lst)
+#t
+(and (NTree? (car lst)) (listNTree? (cdr lst)))))
+
+(define-type Position
+  [2D-Point(x number?) (y number?)])
+
+(define-type Figure
+  [Circle (c Position?) (n number?)]
+  [Square(e Position?) (n number?)]
+  [Rectangle(e Position?) (a number?) (l number?)])
